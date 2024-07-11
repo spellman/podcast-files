8e5bcc7c65e4220288f16bd8bf199dece065809f
Cort Spellman <spellman.cort@gmail.com>
2024-07-10 21:21:27 -0500

    Initial Commit

---
+++ podcast-files/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  podcast-files
+//
+//  Created by Cort Spellman on 7/10/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
